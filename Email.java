c224d832b6cd0e1ce2f8883ac41994e61b7c374c
David Moore <dwjmoore@gmail.com>
2023-03-14 02:09:36 +0000

    Initialized variables. Made outline of methods.

---
+++ Email.java
@@ -0,0 +1,18 @@
+public class Email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+
+	// Constructor to receive first name and last name
+
+	// ask for department
+
+	// generate random password
+
+	// set alternate email
+
+	// change the password
+}
