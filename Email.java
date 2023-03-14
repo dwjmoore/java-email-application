2be60655f6655cacb99b7f8207940103356d7144
David Moore <dwjmoore@gmail.com>
2023-03-14 02:29:12 +0000

    created set department method

--- Email.java
+++ Email.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Email {
 	private String firstName;
 	private String lastName;
@@ -11,9 +13,22 @@ public class Email {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		System.out.println("Email Created: " + this.firstName + " " + this.lastName);
+
+		// Call a method asking for the department - return the department
+		this.department = setDepartment();
+		System.out.println("Depepartment: " + this.department);
 	}
 
 	// ask for department
+	private String setDepartment() {
+		System.out.print("Department Codes:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
+		Scanner in = new Scanner(System.in);
+		int depChoice = in.nextInt();
+		if (depChoice == 1) return "sales";
+		else if (depChoice == 2) return "dev";
+		else if (depChoice == 3) return "acct";
+		else return "";
+	}
 
 	// generate random password
 
